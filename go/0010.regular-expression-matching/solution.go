39cc303e77f7cd9edd32012bba90639c5eec2ed1
luocy <26034847+Luocy7@users.noreply.github.com>
2023-03-31 17:18:42 +0800

    slove no.10

---
+++ go/0010.regular-expression-matching/solution.go
@@ -0,0 +1,92 @@
+// Created by Bob at 2023/03/31 09:35
+// https://leetcode.com/problems/regular-expression-matching/
+
+/*
+10. Regular Expression Matching (Hard)
+Given an input string `s` and a pattern `p`, implement regular expression matching with support for
+`'.'` and `'*'` where:
+
+- `'.'` Matches any single character.
+- `'*'` Matches zero or more of the preceding element.
+
+The matching should cover the **entire** input string (not partial).
+
+**Example 1:**
+
+```
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+```
+
+**Example 2:**
+
+```
+Input: s = "aa", p = "a*"
+Output: true
+Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once,
+it becomes "aa".
+
+```
+
+**Example 3:**
+
+```
+Input: s = "ab", p = ".*"
+Output: true
+Explanation: ".*" means "zero or more (*) of any character (.)".
+
+```
+
+**Constraints:**
+
+- `1 <= s.length <= 20`
+- `1 <= p.length <= 20`
+- `s` contains only lowercase English letters.
+- `p` contains only lowercase English letters, `'.'`, and `'*'`.
+- It is guaranteed for each appearance of the character `'*'`, there will be a previous valid
+character to match.
+
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func isMatch(s string, p string) bool {
+	dp := make([][]bool, len(s)+1)
+	for i := range dp {
+		dp[i] = make([]bool, len(p)+1)
+	}
+	dp[0][0] = true
+
+	for i := 0; i <= len(s); i++ {
+		for j := 1; j <= len(p); j++ {
+			if p[j-1] == '*' {
+				dp[i][j] = dp[i][j-2] || (i > 0 && dp[i-1][j] && (s[i-1] == p[j-2] || p[j-2] == '.'))
+			} else {
+				dp[i][j] = i > 0 && dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '.')
+			}
+		}
+	}
+	return dp[len(s)][len(p)]
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	s := Deserialize[string](ReadLine(stdin))
+	p := Deserialize[string](ReadLine(stdin))
+	ans := isMatch(s, p)
+	fmt.Println("output: " + Serialize(ans))
+}
