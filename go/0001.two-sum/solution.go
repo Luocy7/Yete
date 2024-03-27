f809cef73e2c47af8b0faa018647c0960023ac9a
luocy <26034847+Luocy7@users.noreply.github.com>
2024-03-27 21:37:53 +0800

    add lint config

--- go/0001.two-sum/solution.go
+++ go/0001.two-sum/solution.go
@@ -1,51 +1,6 @@
-// Created by luocy at 2023/03/30 09:35
+// Created by luocy at 2024/03/27 21:23
+// leetgo: 1.4.3
 // https://leetcode.com/problems/two-sum/
-
-/*
-1. Two Sum (Easy)
-Given an array of integers `nums` and an integer `target`, return indices of the two numbers such
-that they add up to `target`.
-
-You may assume that each input would have **exactly one solution**, and you may not use the same
-element twice.
-
-You can return the answer in any order.
-
-**Example 1:**
-
-```
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-```
-
-**Example 2:**
-
-```
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-
-```
-
-**Example 3:**
-
-```
-Input: nums = [3,3], target = 6
-Output: [0,1]
-
-```
-
-**Constraints:**
-
-- `2 <= nums.length <= 10⁴`
-- `-10⁹ <= nums[i] <= 10⁹`
-- `-10⁹ <= target <= 10⁹`
-- **Only one valid answer exists.**
-
-**Follow-up:** Can you come up with an algorithm that is less than `O(n²) ` time complexity?
-
-*/
 
 package main
 
@@ -78,5 +33,6 @@ func main() {
 	nums := Deserialize[[]int](ReadLine(stdin))
 	target := Deserialize[int](ReadLine(stdin))
 	ans := twoSum(nums, target)
-	fmt.Println("output: " + Serialize(ans))
+
+	fmt.Println("\noutput:", Serialize(ans))
 }
--- go/go.mod
+++ go/go.mod
@@ -2,7 +2,6 @@ module leetcode-solutions
 
 go 1.22.1
 
-require (
-	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/j178/leetgo/testutils/go v0.2.0 // indirect
-)
+require github.com/j178/leetgo/testutils/go v0.2.0
+
+require github.com/goccy/go-json v0.10.0 // indirect
